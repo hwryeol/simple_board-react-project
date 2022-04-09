a1e0ae90b14aadf7ed43a3ed1bf33ba4af7a9320
Wonyeol Han <hwryeol@gmail.com>
2022-04-09 21:19:17 +0900

    Revert "Revert "feat: 사용자 정보를 버튼상태에 따라 보이는 기능추가""
    This reverts commit ed3b1b6c63ad1a1cf140131286bc188b0157ea08.

--- src/Header.js
+++ src/Header.js
@@ -5,12 +5,9 @@ import { CgProfile } from "react-icons/cg";
 
 
 function Header({isLogined,setIsLogined, isLoading,getUserData,userData,setUserData}){
-<<<<<<< HEAD
     const [isBtnClick,setIsBtnClick] = useState(false);
     const el = useRef();
 
-=======
->>>>>>> parent of f601803 (feat: 사용자 정보를 버튼상태에 따라 보이는 기능추가)
     function logOut(){
     if(window.confirm("로그아웃 하겠습니까?")){
       fetch('/logout',{
@@ -41,22 +38,12 @@ function Header({isLogined,setIsLogined, isLoading,getUserData,userData,setUserD
       <Link className={styles.header_title} to="/">게시판</Link>
       {isLogined?
         <>
-<<<<<<< HEAD
         <CgProfile className={styles.userDropMenuBtn} onClick={()=>setIsBtnClick(true)}/>
         <div ref={el} className={[!isBtnClick&&styles.hidden,styles.header_login_links,styles.col].join(" ")}>
           <div className={styles.message}>{userData.nickname}</div>
           <Link className={`${styles.header_link} ${styles.profile}`} to="/profile">닉네임 변경</Link>
           <button className={styles.logout_btn} onClick={logOut}>로그아웃</button>
                  
-=======
-        <div className={[styles.header_links,styles.col].join(" ")}>
-            <div className={styles.login_link}>
-            <Link className={`${styles.header_link} ${styles.profile}`} to="/profile">닉네임 변경</Link>
-            <button onClick={logOut}>로그아웃</button>
-        </div>
-
-        <div className={styles.message}>{userData.nickname}님 환영합니다.</div>
->>>>>>> parent of f601803 (feat: 사용자 정보를 버튼상태에 따라 보이는 기능추가)
         </div>
         </>:<>
         <div className={styles.header_logout_links}>
